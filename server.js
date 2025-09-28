d435e76cdd988899d001aae8734c2ffd9fb99128
KawaiiPanicX <mdshahriarsajib47309185@gmail.com>
2025-09-28 09:36:12 +0600

    Add files via upload

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ const User = mongoose.model("User", UserSchema);
 
 // --- Register Route ---
 app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-
+  const { paypass, email, password } = req.body;
+  const nextw=0,processing=0,spint=0,topupt=0;
   // Check if email already exists
   const existingUser = await User.findOne({ email });
   if (existingUser) {
@@ -31,7 +31,7 @@ app.post("/register", async (req, res) => {
   }
 
   // Save new user
-  const user = new User({ name, email, password });
+  const user = new User({ paypass, email, password,nextw,processing,spint,topupt });
   await user.save();
 
   res.json({ success: true, message: "User registered successfully" });
@@ -48,6 +48,52 @@ app.post("/login", async (req, res) => {
 
   res.json({ success: true, message: "Login successful", user });
 });
+
+// Update name route
+app.post('/update-pro', async (req, res) => {
+  try {
+    const { email, password, pro } = req.body;
+
+    // Find user by email + password
+    const user = await User.findOne({ email, password });
+
+    if (!user) {
+      return res.json({ success: false, message: "Invalid email or password" });
+    }
+
+    // Update the name
+    user.processing = pro;
+    await user.save();
+
+    res.json({ success: true, message: "Name updated successfully", user });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Error updating name", error: err.message });
+  }
+});
+
+app.post('/update-pro-spin', async (req, res) => {
+  try {
+    const { email, password, pro,spin } = req.body;
+
+    // Find user by email + password
+    const user = await User.findOne({ email, password });
+
+    if (!user) {
+      return res.json({ success: false, message: "Invalid email or password" });
+    }
+
+    // Update the name
+    user.processing = pro;
+    user.spint = spin;
+    await user.save();
+
+    res.json({ success: true, message: "Name updated successfully", user });
+  } catch (err) {
+    res.status(500).json({ success: false, message: "Error updating name", error: err.message });
+  }
+});
+
+
 
 // --- Start Server ---
 app.listen(5000, () => {
